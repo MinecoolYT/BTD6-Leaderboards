8bee2d2d31f59e1b3924fbf8f7267d8e41ce33d6
MinecoolYT <72187396+MinecoolYT@users.noreply.github.com>
2023-05-10 23:11:22 -0500

    Allowing you to switch leaderboards.. lol

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+let format = true;
+
+/*
+EXPERIMENTAL STUFF
+let flagTime = 0;
+
+async function handleFlagTime(leaderboard, leaderboardKey) {
+    if (leaderboardKey == 'leaderboard') {
+        console.log(leaderboard);
+        // const data = await fetch(leaderboard);
+    }
+    if (leaderboardKey == 'leaderboard_standard_players_1') {
+        if (leaderboard.bossType == 'bloonarius') flagTime = 34250;
+        if (leaderboard.bossType == 'lych') flagTime = 186660;
+        if (leaderboard.bossType == 'vortex') flagTime = 36250;
+        if (leaderboard.bossType == 'dreadbloon') flagTime = 34250;
+    }
+    if (leaderboardKey == 'leaderboard_elite_players_1') {
+        if (leaderboard.bossType == 'bloonarius') flagTime = 30250;
+        if (leaderboard.bossType == 'lych') flagTime = 246850;
+        if (leaderboard.bossType == 'vortex') flagTime = 36250;
+        if (leaderboard.bossType == 'dreadbloon') flagTime = 34250;
+    }
+}
+*/
+
 async function addPlayer(nameArg, timeArg) {
     const placement = document.createElement('div');
     const rank = document.createElement('div');
@@ -8,85 +34,83 @@ async function addPlayer(nameArg, timeArg) {
     placement.className = 'placement';
     rank.className = 'rank';
     name.className = 'name';
-    badges.className = 'badges';
     time.className = 'time';
 
     rank.textContent = document.querySelector('.leaderboard').childElementCount + 1;
     name.textContent = nameArg;
+    // if (timeArg < flagTime) {
+    //     placement.style.backgroundColor = 'red';
+    // }
     badges.textContent = 'N/A';
     time.textContent = timeArg;
 
     placement.appendChild(rank);
     placement.appendChild(name);
-    // placement.appendChild(badges);
     placement.appendChild(time);
 
     document.querySelector('.leaderboard').appendChild(placement);
 }
-async function makeRequest(url, format) {
+async function makeRequest(url) {
     const data = await fetch(url);
     const json = await data.json();
     const body = json.body;
     if (format) {
         for (i = 0; i < body.length; i++) {
-            addPlayer(body[i].displayName, new Date(body[i].score).toISOString().slice(14, 23))
+            addPlayer(body[i].displayName, new Date(body[i].score).toISOString().slice(14, 23));
         }
     } else {
         for (i = 0; i < body.length; i++) {
-            addPlayer(body[i].displayName, body[i].score)
+            addPlayer(body[i].displayName, body[i].score);
         }
     }
-    if (json.next) {
-        await makeRequest(json.next, format);
-    } else {
-        loading = false;
-    }
+    await makeRequest(json.next);
 }
 
-async function main(url, totalKey, leaderboardKey, format) {
-    loading = true;
-    const data = await fetch(url);
+async function main(url, totalKey, leaderboardKey) {
+    const data = await fetch(`https://data.ninjakiwi.com/btd6/${url}`);
     const json = await data.json();
     const currentLeaderboard = json.body.filter(event => event.start < Date.now())[0];
+    //handleFlagTime(currentLeaderboard, leaderboardKey);
     document.querySelector(".playerCount").innerText = `${currentLeaderboard[totalKey]}\nTotal\nEntries`;
-    makeRequest(currentLeaderboard[leaderboardKey], format);
+    makeRequest(currentLeaderboard[leaderboardKey]);
 }
-
-let prevButton = null;
-let loading = false;
 
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.btn');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            if (loading) return alert('Please refresh the page or wait for the current leaderboard to finish before switching to another one.');
-            document.getElementsByClassName('leaderboard')[0].innerHTML = ''
-            if (prevButton !== null) prevButton.style.backgroundColor = '';
-            prevButton = button;
-            button.style.backgroundColor = 'yellow';
-            document.getElementsByClassName('label')[1].innerText = 'Player Name';
-            document.getElementsByClassName('label')[6].innerText = 'Best Time';
-            switch (button.id) {
-                case 'race':
-                    main('https://data.ninjakiwi.com/btd6/races', 'totalScores', 'leaderboard', true);
-                    break;
-                case 'normalboss':
-                    main('https://data.ninjakiwi.com/btd6/bosses', 'totalScores_standard', 'leaderboard_standard_players_1', true);
-                    break;
-                case 'eliteboss':
-                    main('https://data.ninjakiwi.com/btd6/bosses', 'totalScores_elite', 'leaderboard_elite_players_1', true);
-                    break;
-                case 'ctplayers':
-                    document.getElementsByClassName('label')[6].innerText = 'Score';
-                    main('https://data.ninjakiwi.com/btd6/ct', 'totalScores_player', 'leaderboard_player', false);
-                    break;
-                case 'ctteams':
-                    document.getElementsByClassName('label')[1].innerText = 'Team Name';
-                    document.getElementsByClassName('label')[6].innerText = 'Score';
-                    main('https://data.ninjakiwi.com/btd6/ct', 'totalScores_team', 'leaderboard_team', false);
-                    break;
-            }
+            window.location.replace(`./?lb=${button.id}`)
         });
-    });
-    // main();
-})
+    })
+    const currentLeaderboard = new URLSearchParams(window.location.search).get('lb') || null;
+    if (currentLeaderboard === null) return;
+    const button = document.getElementById(currentLeaderboard);
+    button.style.backgroundColor = 'yellow';
+    document.getElementsByClassName('label')[1].innerText = 'Player Name';
+    document.getElementsByClassName('label')[6].innerText = 'Best Time';
+    switch (button.id) {
+        case 'race':
+            format = true;
+            main('races', 'totalScores', 'leaderboard');
+            break;
+        case 'normalboss':
+            format = true;
+            main('bosses', 'totalScores_standard', 'leaderboard_standard_players_1');
+            break;
+        case 'eliteboss':
+            format = true;
+            main('bosses', 'totalScores_elite', 'leaderboard_elite_players_1');
+            break;
+        case 'ctplayers':
+            format = false;
+            document.getElementsByClassName('label')[6].innerText = 'Score';
+            main('ct', 'totalScores_player', 'leaderboard_player');
+            break;
+        case 'ctteams':
+            format = false;
+            document.getElementsByClassName('label')[1].innerText = 'Team Name';
+            document.getElementsByClassName('label')[6].innerText = 'Score';
+            main('ct', 'totalScores_team', 'leaderboard_team');
+            break;
+    }
+});
